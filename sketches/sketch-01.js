85402ee86a766e7a32a4e01d4df6c5066fb54536
ivan7182 <muhammadivan7182@gmail.com>
2025-09-09 21:09:46 +0700

    add color

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const Color = require('canvas-sketch-util/color');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -6,12 +7,11 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'green';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
     context.lineWidth = width * 0.01;
-    context.strokeStyle = 'white';
-    context.stroke();
-
+    context.strokeStyle = 'hsla(300, 77%, 42%, 1.00)';
+    
     const w = width * 0.10;
     const h = height * 0.10;
     const gap = width * 0.03;
@@ -22,28 +22,30 @@ const sketch = () => {
 
     let x,y;
 
-    for (let i = 0; i < 5; i ++) {
-      for (let j = 0; j < 5; j++) {
-        x = ix + (w + gap) * i;
-        y = iy + (h + gap) * j;
+    for (let i = 0; i < 5; i++) {
+        for (let j =0; j < 5; j++) {
+             x = ix  + (w + gap) * i ;
+             y = iy  + (h + gap ) * j;
 
-        context.beginPath();
-        context.fillStyle = 'black';
-        context.rect(x,y,w,h);
-        context.fill();
-        context.stroke();
+            const randomColor = Color.offsetHSL('crimson', Math.random() * 360, 0, 0).hex;
+            context.strokeStyle = randomColor;
+            context.fillStyle = randomColor;
 
-        if(Math.random() > 0.5) {
-          context.beginPath();
-          context.fillStyle = 'red'
-          context.rect(x + off / 2, y + off / 2, w - off, h - off);
-          context.stroke();
-          context.fill();
+            if (Math.random() > 0.5) {
+                context.beginPath();
+                context.rect(x + off / 2, y + off / 2, w - off, h - off);
+                context.fillStyle = 'crimpson';
+                context.fill();
+                context.stroke();
+            } else {
+                context.beginPath();
+                context.arc(x + w / 2, y + h / 2 , 30 , 0 , Math.PI * 2);
+                context.fillStyle = 'crimpson';
+                context.fill();
+                context.stroke();
+            }
         }
       }
-    }
-
-         
   };
 };
 
